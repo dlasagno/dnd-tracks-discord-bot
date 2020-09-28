cb5d3e1bbd3117a9d01814a0c74675892ea36068
Daniele Lasagno <danielelasagno@gmail.com>
2020-09-28 10:15:52 +0200

    Ported list command

---
+++ commands/list/list.js
@@ -0,0 +1,66 @@
+const { Command } = require("discord.js-commando");
+const tracksManager = require("../../src/tracks-manager");
+const messageFormatter = require("../../src/message-formatter");
+const ytdl = require("ytdl-core");
+
+async function getTrackTitle(url, linked = true) {
+  let trackTitle = (await ytdl.getBasicInfo(url)).videoDetails.title;
+  if (linked) trackTitle = `[${trackTitle}](${url})`;
+
+  return trackTitle;
+}
+
+module.exports = class ListCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "list",
+      aliases: ["ls"],
+      group: "list",
+      memberName: "list",
+      description: "Lists the available tracks.",
+      args: [
+        {
+          key: "trackName",
+          prompt: "The name of the track",
+          type: "string",
+          default: "",
+        },
+      ],
+      throttling: {
+        usages: 1,
+        duration: 1,
+      },
+    });
+  }
+
+  async run(message, { trackName }) {
+    const trackUrls = tracksManager.getUrls();
+
+    if (trackName in trackUrls) {
+      message.embed(
+        messageFormatter
+          .getBaseMessage()
+          .addField(
+            `Tracks for ${trackName}`,
+            (
+              await Promise.all(
+                trackUrls[trackName].map(
+                  async (url, i) => `${i} - ${await getTrackTitle(url)}`
+                )
+              )
+            ).join("\n")
+          )
+      );
+    } else {
+      message.embed(
+        messageFormatter.getBaseMessage().addField(
+          "List of tracks",
+          `\n${Object.entries(tracksManager.getUrls())
+            .map(([key, value]) => `${key} (${value.length})`)
+            .sort()
+            .join("\n")}`
+        )
+      );
+    }
+  }
+};
